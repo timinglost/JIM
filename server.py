15dbd32e16de41630a67d9950439ee161ecaac95
Kirill <budanovkirilldmitrievich@gmail.com>
2021-12-28 20:07:23 +0300

    dz

--- server.py
+++ server.py
@@ -1,16 +1,60 @@
 from socket import socket, AF_INET, SOCK_STREAM
-import time, json
+import time, json, sys
+from utils import *
 
-s = socket(AF_INET, SOCK_STREAM)
-s.bind(('', 8888))
-s.listen(5)
 
-while True:
-    client, addr = s.accept()
-    client_data = json.loads(client.recv(1000000).decode("utf-8"))
-    if client_data['action'] == 'presence':
-        answer = {"response": '200',
-                  "time": time.time(),
-                  "alert": "успешное завершение"}
-        client.send(json.dumps(answer).encode('utf-8'))
-    client.close()
+def massege_treatment(massage, config):
+    if config['ACTION'] in massage \
+            and massage[config['ACTION']] == config['PRESENCE'] \
+            and config['TIME'] in massage \
+            and config['USER'] in massage \
+            and massage[config['USER']][config['ACCOUNT_NAME']] == 'Ivan':
+        return {"response": '200',
+                "time": time.time(),
+                "alert": "успешное завершение"}
+    return {"response": '400',
+            "time": time.time(),
+            "alert": "Неверные данные"}
+
+
+def main():
+    config = load_config()
+    try:
+        if '-p' in sys.argv:
+            port = int(sys.argv[sys.argv.index('-p') + 1])
+        else:
+            port = config['DEFAULT_PORT']
+        if not 65535 >= port >= 1024:
+            raise ValueError
+    except IndexError:
+        print('После -\'p\' необходимо указать порт')
+        sys.exit(1)
+    except ValueError:
+        print(
+            'Порт должен быть указан в пределах от 1024 до 65535')
+        sys.exit(1)
+
+    try:
+        if '-a' in sys.argv:
+            address = sys.argv[sys.argv.index('-a') + 1]
+        else:
+            address = ''
+
+    except IndexError:
+        print(
+            'После \'a\'- необходимо указать адрес для ')
+        sys.exit(1)
+    s = socket(AF_INET, SOCK_STREAM)
+    s.bind((address, port))
+    s.listen(config['MAX_CONNECTIONS'])
+
+    while True:
+        client, addr = s.accept()
+        client_data = get_data(client, config)
+        answer = massege_treatment(client_data, config)
+        post_data(client, answer, config)
+        client.close()
+
+
+if __name__ == '__main__':
+    main()
